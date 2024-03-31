2bd6f131e82fbf53fb6973b239fd7e7b52d08f27
RicardoJimenez <ricardo.jimenez@escuelaestech.es>
2024-03-31 20:44:04 +0200

    Se actualiza el texto del print

--- hellogit.java
+++ hellogit.java
@@ -1,7 +1,6 @@
 public class hellogit {
 
     public static void main(String[] args) {
-        System.out.println("Hola Git");
-        System.out.println("¿Qué tal el curso?");
+        System.out.println("Hello Git");
     }
 }
